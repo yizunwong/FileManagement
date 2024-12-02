c517f182a4889d3a6e082a164e5afdd98e6219e4
yizunwong <yizunwong@gmail.com>
2024-12-02 16:51:38 +0800

    done fixing public issue

--- file-management/src/app.js
+++ file-management/src/app.js
@@ -1,26 +1,34 @@
-import express from 'express'
-import cors from 'cors'
-import fileRoutes from './router/fileRoutes.js'
-import authRotes from './router/authRoutes.js'
-import cookieParser from "cookie-parser";
+import express from 'express';
+import cors from 'cors';
+import fileRoutes from './router/fileRoutes.js';
+import authRoutes from './router/authRoutes.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
 
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const app = express()
-const PORT = 3000
-
+// Middleware
 app.use(
   cors({
     origin: 'http://localhost:5173',
     credentials: true,
   }),
-)
-app.use(cookieParser())
-app.use(express.json({ limit: '100mb' }))
-app.use(express.urlencoded({ limit: '100mb', extended: true }))
+);
+app.use(cookieParser());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
-app.use('/api', fileRoutes)
-app.use('/api', authRotes)
+app.use('/api', fileRoutes);
+app.use('/api', authRoutes);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
-})
+  console.log(`Server running on http://localhost:${PORT}`);
+});
